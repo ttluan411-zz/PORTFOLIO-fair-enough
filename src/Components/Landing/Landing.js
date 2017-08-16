621c7a4afc101361c8b8e5215de97f32169175d1
Logan Truong <ttluan411@gmail.com>
2017-08-16 01:55:07 -0600

    css

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -8,23 +8,30 @@ export default function Landing(props){
         <div className="landing-wrapper">
             <div className="header">F A I R E N O U G H</div>
             <div className="logobox">
-            <Typist>
-                    Animate text.
-            </Typist>
-                <div className="logo">
+              <Typist
+                className="auto-type"
+                avgTypingSpeed={1}
+                startDelay={2500}
+              >
+                  Takes the trouble out of sharing expenses with anyone.
+              </Typist>
 
-                     <img src="./Money-Icon.png" alt="logo"/>
-                </div>
             </div>
-
-            <div className="account">
-            <a className="Login-button" href="http://localhost:3001/auth">
-                <button className="log-in">
-                    SIGN IN
-                    {/*<div className="arrow-button"><img src="./a.png" alt="arrow"/></div>*/}
-                </button>
-            </a>
-
+            <div className="button-box">
+              <div className="shadow"></div>
+              <a className="Login-button" href="http://localhost:3001/auth">
+                  <div className="log-in">
+                      GET STARTED
+                  </div>
+                  // <div class="wrapper">
+                  //   <a href="#" class="btn">Hover Me</a>
+                  // </div>
+              </a>
+            </div>
+            <div className="service-box">
+              <div className="box 1"></div>
+              <div className="box 2"></div>
+              <div className="box 3"></div>
             </div>
         </div>
     )
